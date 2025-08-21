a029ab0c2ae5bc2248f25534ef7b64a71c13bc0d
Gabriel <gabriel@MacBook-Pro-de-Nicoli.local>
2025-08-21 11:18:31 -0300

    feat: added get task by id route

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const task = await TaskModel.findById(taskId);
+        if (!task) {
+            return res.status(404).send("Task not found");
+        }
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     try {
         const newTask = new TaskModel(req.body);
