4e3c79c674b33e7e208a07edf491522e2718daff
Gabriel <gabriel@MacBook-Pro-de-Nicoli.local>
2025-08-21 10:00:16 -0300

    feat: add TaskModel

---
+++ src/models/task.model.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const TaskSchema = Schema({
+    description: {
+        type: String,
+        required: true,
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const TaskModel = model("Task", TaskSchema);
+
+export default TaskModel;
