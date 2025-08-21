c433c0c2b08083cc7b4621071f7cca84d6babbae
Gabriel <gabriel@MacBook-Pro-de-Nicoli.local>
2025-08-21 14:34:54 -0300

    feat: moved Task logic to Controllers

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,90 @@
+import TaskModel from "../models/task.model.js";
+
+class TaskController {
+    constructor(req, res) {
+        this.req = req;
+        this.res = res;
+    }
+
+    async getTasks() {
+        try {
+            const tasks = await TaskModel.find({});
+            this.res.status(200).send(tasks);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async getTaskById() {
+        try {
+            const taskId = this.req.params.id;
+            const task = await TaskModel.findById(taskId);
+            if (!task) {
+            }
+            return this.res.status(404).send("Task not found");
+            this.res.status(200).send(task);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async createTask() {
+        try {
+            const newTask = new TaskModel(this.req.body);
+            await newTask.save();
+
+            this.res.status(201).send(newTask);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async deleteTask() {
+        try {
+            const taskId = this.req.params.id;
+            const taskToDelete = await TaskModel.findById(taskId);
+            if (!taskToDelete) {
+                return this.res.status(500).send("Task Not found");
+            }
+
+            const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+            this.res.status(200).send(deletedTask);
+        } catch (error) {
+            this.res.status(500).send(error.message);
+        }
+    }
+
+    async updateTask() {
+        try {
+            const taskId = this.req.params.id;
+            const taskToUpdate = await TaskModel.findById(taskId);
+
+            if (!taskToUpdate) {
+                return this.res.status(500).send("Task Not found");
+            }
+
+            const allowedUpdates = ["isCompleted"];
+            const requestedUpdates = Object.keys(this.req.body);
+
+            for (const update of requestedUpdates) {
+                if (allowedUpdates.includes(update)) {
+                    taskToUpdate[update] = this.req.body[update];
+                } else {
+                    return this.res
+                        .status(500)
+                        .send(
+                            `One or more fields are not editable.\nEditable fields: ${allowedUpdates}`
+                        );
+                }
+            }
+
+            await taskToUpdate.save();
+            return this.res.status(200).send(taskToUpdate);
+        } catch (error) {
+            console.log(error);
+            this.res.status(500).send(error.message);
+        }
+    }
+}
+
+export default TaskController;
